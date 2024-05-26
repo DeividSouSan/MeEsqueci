d1e7c39921735ac2c9a273ad1eb1f74b74c8b87e
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-05-26 09:19:33 -0300

    refactor: :recycle: Refactored code to handle the create app factory.

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-from src import app
+from flask import current_app
+
+from src import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8080, debug=True)
