49b3d67d6745a04b70f0fd49ad06d751d618484c
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-06-10 06:39:27 -0300

    feat: :sparkles: Added bluepring routes to app.

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -1,39 +1,97 @@
-from flask import Blueprint, redirect, render_template, session, url_for
+from flask import Blueprint, Response, redirect, render_template, session, url_for
 
-from ..forms.category_form import CategoryForm
+from src.entities.category import Category
+from src.forms.category_form import CategoryForm
+from src.repositories.category_repository import CategoryRepository
+from src.use_cases.get_all_categories import GetAllCategories
+from src.use_cases.save_categories_to_database import SaveCategoriesToDatabase
 
 main_bp = Blueprint("main", __name__)
+
+
+def redirect_response(route: str):
+    response = Response()
+    response.headers["hx-redirect"] = url_for(route)
+    return response
+
+
+# View
 
 
 @main_bp.route("/")
 def home():
     form = CategoryForm()
-    return render_template("home.html", form=form)
+
+    if "categories" not in session:
+        print("Não existe o item 'categories' na sessão. Criando...")
+        session["categories"] = []
+
+    if "categories_objects" not in session:
+        print("Não existe o item 'categories_objects'. Criando... ")
+        session["categories_objects"] = []
+
+    return render_template(
+        "home.html", form=form, categories=session["categories_objects"]
+    )
+
+
+# Sessão
 
 
 @main_bp.route("/add-cat-to-session", methods=["POST"])
 def add_cat_to_session():
     form = CategoryForm()
-    form["name"]
 
-    if "categories" not in session:
-        print("Não existe o item 'categories' ")
-        session["categories"] = []
+    last_category = Category(form.data["name"])
 
-    session["categories"].append(form.data["name"])
+    session["categories"].append(last_category.__dict__)
     session.modified = True  # Como a lista é uma estrutura mutável, acaba que o flask não percebe que o conteúdo da lista foi modificado pois ela permanece sendo uma lista
 
-    return render_template("categories-list.html", categories=session["categories"])
+    return render_template(
+        "partials/categories-list.html", categories=session["categories"]
+    )
 
 
-@main_bp.route("/clear_session", methods=["POST"])
-def clear_session():
-    # Todo: a sessão está sendo limpa, mas o html não é atualizado
-    session.clear()
+# Banco de Dados
+
+
+@main_bp.route("/save-categories-to-database", methods=["POST"])
+def save_categories_to_database():
+
+    repository = CategoryRepository()
+    use_case = SaveCategoriesToDatabase(repository)
+
+    use_case.execute(session["categories"])
+
+    session["categories"] = []
+    session.modified = True
+
+    return redirect(url_for("main.get_categories"))
+
+
+@main_bp.route("/clear-categories", methods=["GET", "POST"])
+def clear_categories():
+    session["categories"] = []
 
     return "", 204
 
 
-"""
-O que acontece é o seguinte, eu vou bater na rota add_cat_to_session e não vou fazer redirecionamento nenhum, eu vou apenas acessar uma variável com todos as cat adicionadas até agora e usar elas para alterar a segunda parte do modal. Depois, quando todas as cetegoria que eu quiser estiverem adicionadas o 'save change' vai bater em outra rota que vai adicionar as categorias ao banco de dados.
-"""
+@main_bp.route("/get-categories", methods=["GET", "POST"])
+def get_all_categories():
+
+    repository = CategoryRepository()
+    use_case = GetAllCategories(repository)
+
+    categories_list = use_case.execute()
+
+    return render_template("partials/categories-board.html", categories=categories_list)
+
+
+@main_bp.route("/add-item-to-category", methods=["POST"])
+def add_item_to_category():
+    if "items" not in session:
+        session["items"] = []
+
+    session["items"].append("item")
+
+    return render_template("partials/items-list.html", items=session["items"])
