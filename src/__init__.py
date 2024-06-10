56ca227eeb0fafb89e7efcac93b519c8b83826f1
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-06-10 06:36:47 -0300

    refactor: :card_file_box: Change from SQLite to TinyDB

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,13 @@
 import os
-import sqlite3
 
 from flask import Flask, g
-from flask_bootstrap import Bootstrap5
-
-bootstrap = Bootstrap5()
+from tinydb import TinyDB
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    bootstrap.init_app(app)
+    db = TinyDB("db.json")
+
     try:
         os.makedirs(app.instance_path)
     except OSError as err:
@@ -22,47 +20,7 @@ def create_app():
         SECRET_KEY="12345678", DATABASE=os.path.join(app.instance_path, "database.db")
     )
 
-    # todo: Colocar esses métodos do banco de dados em uma classe ou arquivo diferente
-    def get_db():
-        if "db" not in g:
-            g.db = sqlite3.connect(
-                app.config["DATABASE"],
-            )
-
-        return g.db
-
-    @app.teardown_appcontext
-    def close_db(exception):
-        db = g.pop("db", None)
-
-        if db is not None:
-            db.close()
-
-    # todo: recriar o banco de dados pois adicionei o status 4 depois de ter criado o bd
-    def init_db():
-        db = get_db()
-        db.executescript(
-            """
-            CREATE TABLE IF NOT EXISTS Categories (
-                idCategory INTEGER NOT NULL,
-                name TEXT NOT NULL,
-                PRIMARY KEY(idCategory)
-            );
-
-
-            CREATE TABLE IF NOT EXISTS Items (
-                idItem INTEGER NOT NULL,
-                name TEXT NOT NULL,
-                status INTEGER NOT NULL,
-
-                PRIMARY KEY(idItem)
-                CHECK(status IN (1, 2, 3, 4))
-            );"""
-        )
-
     with app.app_context():
-        init_db()
-
         from .routes.routes import main_bp
 
         app.register_blueprint(main_bp)
